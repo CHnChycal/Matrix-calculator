d2ac69214564625621ebdc554f2d5b1a14d354de
letmeseeseeee <1390122854@qq.com>
2023-10-26 22:08:16 +0800

    行列式计算

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -28,34 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.count = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // textBox1
             // 
-            this.button1.Location = new System.Drawing.Point(713, 415);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "退出";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.textBox1.Location = new System.Drawing.Point(81, 64);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(520, 25);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(304, 288);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "退出";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // count
+            // 
+            this.count.Location = new System.Drawing.Point(105, 288);
+            this.count.Name = "count";
+            this.count.Size = new System.Drawing.Size(75, 23);
+            this.count.TabIndex = 4;
+            this.count.Text = "计算";
+            this.count.UseVisualStyleBackColor = true;
+            this.count.Click += new System.EventHandler(this.count_Click);
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.count);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
             this.Name = "Form2";
             this.Text = "行列式计算";
             this.Load += new System.EventHandler(this.Form2_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button count;
     }
 }
--- Form2.cs
+++ Form2.cs
@@ -22,10 +22,97 @@ namespace Matrix_calculator
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        //private void button1_Click(object sender, EventArgs e)
+        //{
+           // Close();
+       // }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void count_Click(object sender, EventArgs e)
         {
             Form1.fmm.Show();
-            Close();
+            try
+            {
+                string input = textBox1.Text;
+                string[] rows = input.Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+                int n = rows.Length;
+                double[,] matrix = new double[n, n];
+
+                for (int i = 0; i < n; i++)
+                {
+                    string[] cols = rows[i].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                    if (cols.Length != n)
+                    {
+                        throw new Exception("Invalid matrix format. Please enter a valid square matrix.");
+                    }
+                    for (int j = 0; j < n; j++)
+                    {
+                        matrix[i, j] = Convert.ToDouble(cols[j]);
+                    }
+                }
+
+                Matrix mat = new Matrix(matrix);
+                double determinant = mat.CalculateDeterminant();
+                MessageBox.Show("Determinant: " + determinant, "Result");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Error");
+            }
+        }
+    }
+    public class Matrix
+    {
+        private double[,] matrix;
+        private int size;
+
+        public Matrix(double[,] inputMatrix)
+        {
+            matrix = inputMatrix;
+            size = (int)Math.Sqrt(matrix.Length);
+        }
+
+        public double CalculateDeterminant()
+        {
+            if (size == 1)
+            {
+                return matrix[0, 0];
+            }
+            else if (size == 2)
+            {
+                return (matrix[0, 0] * matrix[1, 1]) - (matrix[1, 0] * matrix[0, 1]);
+            }
+            else
+            {
+                double det = 0;
+                for (int k = 0; k < size; k++)
+                {
+                    Matrix subMatrix = CreateSubMatrix(matrix, size, k);
+                    det += (k % 2 == 1 ? -1 : 1) * matrix[0, k] * subMatrix.CalculateDeterminant();
+                }
+                return det;
+            }
+        }
+
+        private Matrix CreateSubMatrix(double[,] matrix, int n, int k)
+        {
+            double[,] subMatrix = new double[n - 1, n - 1];
+            int p = 0;
+            for (int i = 1; i < n; i++)
+            {
+                int q = 0;
+                for (int j = 0; j < n; j++)
+                {
+                    if (j == k) continue;
+                    subMatrix[p, q] = matrix[i, j];
+                    q++;
+                }
+                p++;
+            }
+            return new Matrix(subMatrix);
         }
     }
 }
